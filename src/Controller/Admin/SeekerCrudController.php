f0a1640f819618faafef2ea463279f02bbe417f0
MarwenTlili <marwentlili@gmail.com>
2021-08-07 19:38:40 +0100

    upload

--- src/Controller/Admin/SeekerCrudController.php
+++ src/Controller/Admin/SeekerCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Seeker;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class SeekerCrudController extends AbstractCrudController
 {
@@ -12,14 +16,17 @@ class SeekerCrudController extends AbstractCrudController
         return Seeker::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('firstName'),
+            TextField::new('lastName'),
+            TextField::new('civility'),
+            DateField::new('birthDate'),
+            TextField::new('city'),
+            AssociationField::new('user'),
+            AssociationField::new('country'),
         ];
     }
-    */
 }
